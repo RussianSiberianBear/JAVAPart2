bdd3b3a04acb4f91e7dc4ff288572f7599e43b63
Alex <alexbaturin@list.ru>
2025-10-24 21:06:08 +0800

    Part2 HW2

---
+++ src/org/skypro/skyshop/Product.java
@@ -0,0 +1,33 @@
+package org.skypro.skyshop;
+
+public class Product {
+    private String productName;
+    private int productPrice;
+
+    public Product(String productName, int productPrice) {
+        if (productName == null || productName.isBlank()) {
+            throw new IllegalArgumentException("Имя продукта не может быть пустым!");
+        }
+        if (productPrice <= 0) {
+            throw new IllegalArgumentException("Цена продукта не может быть 0 или отрицательной!");
+        }
+        this.productName = productName.trim();
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return this.productName;
+    }
+
+    public int getProductPrice() {
+
+        return this.productPrice;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.productName + ": " + productPrice;
+    }
+
+}
---
+++ src/org/skypro/skyshop/ProductBasket.java
@@ -0,0 +1,77 @@
+package org.skypro.skyshop;
+
+public class ProductBasket {
+
+    private int cntProductBasket;
+
+    private Product[] products;
+
+    public ProductBasket(int size) {
+        this.products = new Product[size];
+        this.cntProductBasket = 0;
+    }
+
+
+    public boolean isProductExistsByName(String productName) {
+
+        if (this.cntProductBasket == 0) {
+            return false;
+        }
+        for (Product product : this.products) {
+            if (product != null && product.getProductName().equals(productName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean productAdd(Product product) {
+        if (this.cntProductBasket == this.products.length) {
+            return false;
+        }
+        for (int i = 0; i < this.products.length; i++) {
+            if (this.products[i] == null) {
+                this.products[i] = product;
+                this.cntProductBasket++;
+                break;
+            }
+        }
+        return true;
+    }
+
+
+    public int getTotalBasketCost() {
+        int sum = 0;
+        for (Product product : this.products) {
+            if (product != null) {
+                sum += product.getProductPrice();
+            }
+        }
+        return sum;
+    }
+
+
+    @Override
+    public String toString() {
+        String result = "";
+        if (this.cntProductBasket == 0) {
+            return ("В корзине пусто!");
+        }
+
+        for (Product product : this.products) {
+            if (product != null) {
+                result += "\n" + product;
+            }
+        }
+        result += "\nИтого: " + this.getTotalBasketCost();
+        return result;
+    }
+
+    public void empty() {
+        this.products = new Product[5];
+        this.cntProductBasket = 0;
+    }
+
+
+}
